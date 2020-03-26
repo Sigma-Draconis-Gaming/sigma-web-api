d609437a35c30291ee2a05106dda3afb3e48c95a
Tails <hydrabott@gmail.com>
2020-03-26 15:01:07 -0400

    key

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def update_thread():
             ws.emit('votes_update', {'server': cat, "votes": get_vote_data(cat)["votes"]}, namespace='/wss')
             for server in get_steam_data(cat)[cat]:
                 t = sum([int(x['players']) for x in server])
-                ws.emit("online_update", {f'{cat}_count': t}, namespace='/wss')
+                ws.emit("online_update", {'id_key': f'{cat}_count', 'count': t}, namespace='/wss')
                 ws.emit('server_update', {'server': server}, namespace='/wss')
                 ws.sleep(.1)
 
